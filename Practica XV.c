9f19e54debf660efce7ba5f013d01e6e82230b87
Héctor Leonardo Torres Campos <hector.torres2193@alumnos.udg.mx>
2026-03-25 09:15:09 -0600

    Practica XV.c

--- Practica XV.c
+++ Practica XV.c
@@ -10,13 +10,13 @@ Programa de Busqueda Secuencial
 int main (){
     int arreglo[10] = { 12, 45, 7, 89, 23, 56, 90, 34, 11, 67};
     int numeroABuscar;
-    int encontrado = 0;
-    int posicion = -1;
+    int encontrado = 0; //Inicia en 0 y cambia a 1 si encuentra el numero 
+    int posicion = -1;  // lo hace para que ponga bien la posicion
     
-    printf("Ingrese el numero que desea buscar: ");
+    printf("Ingrese el numero que desea buscar: "); //Aqui ingresas el numero que deseas buscar
     scanf("%d", &numeroABuscar);
     
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 10; i++){          //Aqui va recorriendo uno por uno para revisar si el numero esta en el arreglo
         if(arreglo[i] == numeroABuscar){
             encontrado = 1;
             posicion = i;
@@ -25,10 +25,10 @@ int main (){
         
     }
     if(encontrado == 1){
-        printf("El numero %d fue encontrado en la posicion %d\n",numeroABuscar, posicion);
+        printf("El numero %d fue encontrado en la posicion %d\n",numeroABuscar, posicion);//Aqui te dice si el numero esta y en posicion
     }
     else{
-    printf("El numero %d no fue encontrado en el arreglo\n",numeroABuscar);
+    printf("El numero %d no fue encontrado en el arreglo\n",numeroABuscar);//Aqui te dice si el numero no esta en el arreglo
     }
         
     return 0;
