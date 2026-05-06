7a411e819ddd25ec4b2155a905e037dd32442906
Héctor Leonardo Torres Campos <hector.torres2193@alumnos.udg.mx>
2026-05-06 17:34:41 -0600

    Delete prueba.c

--- prueba.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    char nombre[20];
-    printf("¿Como te llamas, ingeniero? ");
-    scanf("%s", nombre);
-    printf("¡Perfecto, %s! Tu entorno de C esta al 100%%.\n", nombre);
-    return 0;
-}
